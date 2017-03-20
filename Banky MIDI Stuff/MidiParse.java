134a7ea9f606d229298c88af12b746e7c830490c
Sikharin Kitchpanich <bankster007@gmail.com>
2017-03-19 18:06:24 -0800

    Commented and added sharp note check

--- Banky MIDI Stuff/MidiParse.java
+++ Banky MIDI Stuff/MidiParse.java
@@ -11,15 +11,21 @@ public class MidiParse {
 
     ///////////////////////////////////////////////////
     //  PARSING VIA THE STANDARD JAVA SOUND LIBRARY
+    //
+    //  This code is largely based off of code
+    //  written by StackOverflow user Sami Koivu.
+    //
     ///////////////////////////////////////////////////
 
 
     public static final int NOTE_ON = 0x90;
     public static final String[] NOTE_NAMES = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+
     private static String stringOfNotes1 = "";
     private static String stringOfNotes2 = "";
 
     public static void main(String[] args) throws Exception {
+        // User input for the MIDI files.
         System.out.print("Please enter the name of the expert MIDI file: ");
         Scanner keyboard = new Scanner(System.in);
         String filename1 = keyboard.nextLine();
@@ -28,9 +34,13 @@ public class MidiParse {
         Sequence sequence1 = MidiSystem.getSequence(new File(filename1));
         Sequence sequence2 = MidiSystem.getSequence(new File(filename2));
 
+        // The getNotes method is called to get the notes from the
+        // MIDI files and put them all into a single string for
+        // each song.
         stringOfNotes1 = getNotes(sequence1, stringOfNotes1);
         stringOfNotes2 = getNotes(sequence2, stringOfNotes2);
 
+        // Prints the strings of notes.
         System.out.println(stringOfNotes1);
         System.out.println(stringOfNotes2);
 
@@ -51,16 +61,31 @@ public class MidiParse {
                 if (message instanceof ShortMessage) {
                     ShortMessage sm = (ShortMessage) message;
 
-                    //Getting data from played notes
+                    // Getting data from played notes
                     if (sm.getCommand() == NOTE_ON) {
                         int key = sm.getData1();
                         int octave = (key / 12) - 1;
                         int note = key % 12;
                         String noteName = NOTE_NAMES[note];
                         int velocity = sm.getData2();
-                        if (velocity != 0 && velocity > 20) {
+                        // The velocity is the force used to press a key on the keyboard.
+                        // We are assuming that anything below velocity 20 is a mistake
+                        // on the user's end, thus we will filter those out.
+                        if (velocity > 20) {
+                            // Reassigning sharp notes to other letters in order to make the
+                            // comparison algorithm function correctly.
+                            if(noteName.equals("C#"))
+                                noteName = "H";
+                            if(noteName.equals("D#"))
+                                noteName = "I";
+                            if(noteName.equals("F#"))
+                                noteName = "J";
+                            if(noteName.equals("G#"))
+                                noteName = "K";
+                            if(noteName.equals("A#"))
+                                noteName = "L";
                             //System.out.println("@" + event.getTick() + " " + "Note on, " + noteName + octave + " key=" + key + " velocity: " + velocity);
-                            //Adding to the string of notes that represents the whole song
+                            // Adding to the string of notes that represents the whole song
                             noteString = noteString + noteName;
                         }
                     }
